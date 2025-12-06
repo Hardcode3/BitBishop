735abf0be470d1bd115fc6030ecfe060a50aeda6
Baptiste PENOT <103385400+Hardcode3@users.noreply.github.com>
2025-12-06 23:43:49 +0100

    Knight pseudo-legal moves implementation (#48)
    * added board.knights() getter function
    
    * knights move generation

---
+++ src/bitbishop/moves/knight_move_gen.cpp
@@ -0,0 +1,26 @@
+#include <bitbishop/lookups/knight.hpp>
+#include <bitbishop/moves/knight_move_gen.hpp>
+
+void KnightMoveGenerator::generate_pseudo_legal_moves(std::vector<Move>& moves, const Board& board, Color side) {
+  Bitboard knights = board.knights(side);
+  Bitboard empty = board.unoccupied();
+  Bitboard enemy = board.enemy(side);
+
+  // warning: this loop is destructive on Bitboard knights
+  while (auto from_opt = knights.pop_lsb()) {
+    Square from = from_opt.value();
+    Bitboard knight_lookup = Lookups::KNIGHT_ATTACKS[from.value()];
+
+    // Classic knight moves
+    Bitboard knight_moves = knight_lookup & empty;
+    for (Square to : knight_moves) {
+      moves.emplace_back(from, to, std::nullopt, false, false, false);
+    }
+
+    // Knight captures
+    Bitboard knight_captures = knight_lookup & enemy;
+    for (Square to : knight_captures) {
+      moves.emplace_back(from, to, std::nullopt, true, false, false);
+    }
+  }
+}
--- tests/bitbishop/test_board.cpp
+++ tests/bitbishop/test_board.cpp
@@ -151,12 +151,12 @@ TEST(BoardTest, PrintBoard) {
  */
 TEST(BoardTest, PawnsBitboard) {
   Board board;
-  board.set_piece(Squares::A2, Pieces::WHITE_PAWN);
-  board.set_piece(Squares::H7, Pieces::BLACK_PAWN);
 
   Bitboard white_pawns = board.pawns(Color::WHITE);
   Bitboard black_pawns = board.pawns(Color::BLACK);
 
+  EXPECT_EQ(white_pawns.count(), 8);
+  EXPECT_EQ(black_pawns.count(), 8);
   EXPECT_TRUE(white_pawns.test(Square::A2));
   EXPECT_FALSE(white_pawns.test(Square::H7));
   EXPECT_TRUE(black_pawns.test(Square::H7));
@@ -169,16 +169,14 @@ TEST(BoardTest, PawnsBitboard) {
  */
 TEST(BoardTest, KingBitboard) {
   Board board;
-  board.set_piece(Squares::A2, Pieces::WHITE_KING);
-  board.set_piece(Squares::H7, Pieces::BLACK_KING);
 
   Bitboard white_king = board.king(Color::WHITE);
   Bitboard black_king = board.king(Color::BLACK);
 
-  EXPECT_TRUE(white_king.test(Square::A2));
-  EXPECT_FALSE(white_king.test(Square::H7));
-  EXPECT_TRUE(black_king.test(Square::H7));
-  EXPECT_FALSE(black_king.test(Square::A2));
+  EXPECT_EQ(white_king.count(), 1);
+  EXPECT_EQ(black_king.count(), 1);
+  EXPECT_TRUE(white_king.test(Square::E1));
+  EXPECT_TRUE(black_king.test(Square::E8));
 }
 
 /**
@@ -187,16 +185,34 @@ TEST(BoardTest, KingBitboard) {
  */
 TEST(BoardTest, RookBitboard) {
   Board board;
-  board.set_piece(Squares::A2, Pieces::WHITE_ROOK);
-  board.set_piece(Squares::H7, Pieces::BLACK_ROOK);
 
-  Bitboard white_rooks = board.rook(Color::WHITE);
-  Bitboard black_rooks = board.rook(Color::BLACK);
+  Bitboard white_rooks = board.rooks(Color::WHITE);
+  Bitboard black_rooks = board.rooks(Color::BLACK);
 
-  EXPECT_TRUE(white_rooks.test(Square::A2));
-  EXPECT_FALSE(white_rooks.test(Square::H7));
-  EXPECT_TRUE(black_rooks.test(Square::H7));
-  EXPECT_FALSE(black_rooks.test(Square::A2));
+  EXPECT_EQ(white_rooks.count(), 2);
+  EXPECT_EQ(black_rooks.count(), 2);
+  EXPECT_TRUE(white_rooks.test(Square::H1));
+  EXPECT_TRUE(white_rooks.test(Square::A1));
+  EXPECT_TRUE(black_rooks.test(Square::H8));
+  EXPECT_TRUE(black_rooks.test(Square::A8));
+}
+
+/**
+ * @test BoardTest.KnightsBitboard
+ * @brief Ensures that knights() correctly returns the bitboard for each side.
+ */
+TEST(BoardTest, KnightsBitboard) {
+  Board board;
+
+  Bitboard white_knights = board.knights(Color::WHITE);
+  Bitboard black_knights = board.knights(Color::BLACK);
+
+  EXPECT_EQ(white_knights.count(), 2);
+  EXPECT_EQ(black_knights.count(), 2);
+  EXPECT_TRUE(white_knights.test(Squares::B1));
+  EXPECT_TRUE(white_knights.test(Squares::G1));
+  EXPECT_TRUE(black_knights.test(Square::B8));
+  EXPECT_TRUE(black_knights.test(Square::G8));
 }
 
 TEST(BoardTest, WhiteKingsideCastlingRights) {
