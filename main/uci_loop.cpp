b1e4c79438cb6243762254ea97c2d188db21c3ea
Baptiste Penot <baptiste.penot@gmail.com>
2025-12-13 12:49:35 +0100

    uci loop linting

--- main/uci_loop.cpp
+++ main/uci_loop.cpp
@@ -14,10 +14,10 @@ using std::string;
 using std::vector;
 
 // Function to split a string into tokens
-vector<string> split(const string &s) {
+vector<string> split(const string &str) {
   vector<string> tokens;
   string token;
-  istringstream tokenStream(s);
+  istringstream tokenStream(str);
   while (tokenStream >> token) {
     tokens.push_back(token);
   }
@@ -32,17 +32,19 @@ void uci_loop(istream &input = cin, ostream &output = cout) {
   while (getline(input, line)) {
     tokens = split(line);
 
-    if (tokens.empty()) continue;
+    if (tokens.empty()) {
+      continue;
+    }
 
     if (tokens[0] == "uci") {
       // Identify the engine
-      output << "id name ChessEngine" << endl;
-      output << "id author Hardcode" << endl;
+      output << "id name ChessEngine\n";
+      output << "id author Hardcode\n";
       // Send options available
-      output << "uciok" << endl;
+      output << "uciok\n";
     } else if (tokens[0] == "isready") {
       // Engine is ready
-      output << "readyok" << endl;
+      output << "readyok\n";
     } else if (tokens[0] == "ucinewgame") {
       // Reset the engine for a new game
       // TODO: Implement new game initialization
@@ -52,7 +54,7 @@ void uci_loop(istream &input = cin, ostream &output = cout) {
     } else if (tokens[0] == "go") {
       // Start calculating
       // TODO: Implement move calculation
-      output << "bestmove e2e4" << endl;  // Placeholder
+      output << "bestmove e2e4\n";  // Placeholder
     } else if (tokens[0] == "quit") {
       // Exit the program
       break;
