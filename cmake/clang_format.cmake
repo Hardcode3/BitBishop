ca8aaa332a91d228bb8da30811e3614f1e315174
Baptiste Penot <baptiste.penot@gmail.com>
2026-02-05 15:53:32 +0100

    refactoring of the clang format target for clarity

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -1,32 +1,22 @@
 # ------------------------------------------------------------------------------
-# ClangFormat.cmake
-#
 # Defines a custom CMake target `clang-format` to format source files using
 # clang-format if it is available on the system. Automatically finds and formats
-# all .cpp and .hpp files under src/ and include/ unless a custom list of files
-# is provided via CLANG_FORMAT_SOURCES:
-#
-# set(CLANG_FORMAT_SOURCES
-#     ${CMAKE_SOURCE_DIR}/src/main.cpp
-#     ${CMAKE_SOURCE_DIR}/include/utils.hpp
-# )
-# include(cmake/ClangFormat.cmake)
+# all .cpp and .hpp files under src/ and include/ using the `.clang-format`
+# configuration file.
 # ------------------------------------------------------------------------------
 
-# Optional: allow user to override the list of files:
-if(NOT CLANG_FORMAT_SOURCES)
-    file(GLOB_RECURSE CLANG_FORMAT_SOURCES
-        "${CMAKE_SOURCE_DIR}/src/*.cpp"
-        "${CMAKE_SOURCE_DIR}/include/*.hpp"
-        "${CMAKE_SOURCE_DIR}/main/*.cpp"
-    )
-endif()
-
-# Find clang-format binary
 find_program(CLANG_FORMAT_BIN NAMES clang-format)
 
 if(CLANG_FORMAT_BIN)
     message(STATUS "clang-format found: ${CLANG_FORMAT_BIN}")
+
+    if(NOT CLANG_FORMAT_SOURCES)
+        file(GLOB_RECURSE CLANG_FORMAT_SOURCES
+            "${CMAKE_SOURCE_DIR}/src/*.cpp"
+            "${CMAKE_SOURCE_DIR}/include/*.hpp"
+            "${CMAKE_SOURCE_DIR}/main/*.cpp"
+        )
+    endif()
 
     add_custom_target(clang-format
         COMMAND ${CLANG_FORMAT_BIN}
@@ -34,6 +24,7 @@ if(CLANG_FORMAT_BIN)
             -style=file
             ${CLANG_FORMAT_SOURCES}
         COMMENT "Running clang-format"
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         VERBATIM
     )
 else()
