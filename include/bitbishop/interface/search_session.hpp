e0350a19e038ed449b984682db25e92fec638217
Baptiste Penot <baptiste.penot@gmail.com>
2026-04-23 10:47:29 +0200

    following reporter file renaming

--- include/bitbishop/interface/search_session.hpp
+++ include/bitbishop/interface/search_session.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <bitbishop/interface/reporter.hpp>
 #include <bitbishop/interface/search_controller.hpp>
+#include <bitbishop/interface/search_reporter.hpp>
 #include <memory>
 #include <ostream>
 
